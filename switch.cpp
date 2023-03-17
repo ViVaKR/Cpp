cca995d438bb800b7762e0f45bc62c2e31761a26
ViVaKR <admin@vivabj.com>
2023-03-17 11:15:35 +0900

    Add switch & Rename helloworld

---
+++ switch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void start(){
+    
+    int month;
+    cout << "Enter the month (1-12) ";
+    cin >> month;
+    switch (month)
+    {
+    case 1:
+        cout << "";
+        break;
+    
+    default:
+        break;
+    };
+}
