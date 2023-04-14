6d45e1a6f012b7ca04f1e5c929a22dd0c92c37e2
ViVaKR <admin@vivabj.com>
2023-04-14 11:45:43 +0900

    Update VivMain

--- VivMain.cpp
+++ VivMain.cpp
@@ -1,28 +1,72 @@
 #include <iostream>
 #include <string.h>
 
+#include "Demo/Array.cpp"
 #include "Demo/Banking.cpp"
+#include "Demo/DoWhileLoop.cpp"
 #include "Demo/ForLoop.cpp"
 #include "Demo/Function.cpp"
-#include "Demo/PseudoRandom.cpp"
-#include "Demo/DoWhileLoop.cpp"
-#include "Demo/Strings.cpp"
-#include "Demo/WhileLoop.cpp"
-#include "Demo/Switch.cpp"
 #include "Demo/If.cpp"
-#include "Demo/TernaryOperator.cpp"
+#include "Demo/PseudoRandom.cpp"
 #include "Demo/RockPaperScissors.cpp"
+#include "Demo/Strings.cpp"
 #include "Demo/StuduentScore.cpp"
+#include "Demo/Switch.cpp"
+#include "Demo/TernaryOperator.cpp"
+#include "Demo/WhileLoop.cpp"
 
+void PrintMenu();
 using namespace std;
 
 /// @brief ./run <exec number>
-/// @param argc 
-/// @param argv 
-/// @return 
+/// @param argc
+/// @param argv
+/// @return
 int main(int argc, char *argv[]) {
 
+    bool fromShell = true;
     cout << "********** 실행할 프로그램 선택하세요 (번호선택) **********" << endl << endl;
+    PrintMenu();
+
+    long choice;
+
+    do {
+        cout << std::string(60, '*') << endl;
+
+        if (strtol(argv[1], NULL, 10) == 100 || !fromShell) {
+            PrintMenu();
+            cout << "프로그램 번호 선택 >> ";
+            cin >> choice;
+        } else {
+            choice = strtol(argv[1], NULL, 10);
+            fromShell = false;
+        }
+
+        cout << "## You Choice (" << choice << ") ##" << endl;
+        std::cout << endl;
+        switch (choice) {
+            case 1: Switch(); break;
+            case 2: TernaryOperator(); break;
+            case 3: If(); break;
+            case 4: Strings(); break;
+            case 5: WhileLoop(); break;
+            case 6: DoWhileLoop(); break;
+            case 7: ForLoop(); break;
+            case 8: PseudoRandom(); break;
+            case 9: MainFunction(); break;
+            case 10: Banking(); break;
+            case 11: RockPaperScissors(); break;
+            case 12: StudentScore(); break;
+            case 13: Array(); break;
+            case 100: cout << "프로그램을 종료합니다." << endl; break;
+            default: cout << "잘못된 선택입니다." << endl; break;
+        }
+    } while (choice != 100);
+
+    return 0;
+}
+
+void PrintMenu() {
     cout << "(1) Switch" << endl;
     cout << "(2) Ternary Operator" << endl;
     cout << "(3) If" << endl;
@@ -35,36 +79,6 @@ int main(int argc, char *argv[]) {
     cout << "(10) Banking" << endl;
     cout << "(11) RockPaperScissors" << endl;
     cout << "(12) StudentScore" << endl;
+    cout << "(13) Array" << endl;
     cout << "(100) 종료" << endl;
-
-    cout << std::string(60, '*') << endl;
-
-    long choice;
-    if (strtol(argv[1], NULL, 10) == 100) {
-        cout << "프로그램 번호 선택 >> ";
-        cin >> choice;
-    } else {
-        choice = strtol(argv[1], NULL, 10);
-    }
-
-    cout << "## You Choice (" << choice << ") ##" << endl;
-    std::cout << endl;
-    
-    switch (choice) {
-        case 1: Switch(); break;
-        case 2: TernaryOperator(); break;
-        case 3: If(); break;
-        case 4: Strings(); break;
-        case 5: WhileLoop(); break;
-        case 6: DoWhileLoop(); break;
-        case 7: ForLoop(); break;
-        case 8: PseudoRandom(); break;
-        case 9: MainFunction(); break;
-        case 10: Banking(); break;
-        case 11: RockPaperScissors(); break;
-        case 12: StudentScore(); break;
-        case 100: cout << "프로그램을 종료합니다." << endl; break;
-        default: cout << "잘못된 선택입니다." << endl; break;
-    }
-    return 0;
 }
