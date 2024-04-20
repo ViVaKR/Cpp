7caa5766483c5b2ee91e804c707c0014fe52c27d
Kim Bum Jun <admin@vivabj.com>
2024-04-20 11:02:11 +0900

    Rebuild Directory Structure

---
+++ Playground/Demo/main.cpp
@@ -0,0 +1,71 @@
+#include <cmath>
+#include <iostream>
+
+int main(int, char **) {
+    // if statement
+    int age;
+
+    std::cout << "Enter Your Age: ";
+
+    std::cin >> age;
+
+    return 0;
+}
+
+/* --> Code Snippets
+- comment : gcc
+
+
+    ##
+    double x;
+    double y;
+    double z;
+
+    std::cout << "Enter side X: ";
+    std::cin >> x;
+
+    std::cout << "Enter side Y: ";
+    std::cin >> y;
+
+    std::cout << "Z = " << sqrt(pow(x, 2) + pow(y, 2)) << std::endl;
+    std::cout << "Hello, World" << std::endl;
+    ##
+    z = std::max(x, y);
+    z = std::min(x, y);
+    z = pow(2, 4);
+    z = sqrt(9);
+    z = abs(-3);
+    z = round(x);
+    z = ceil(x);
+    z = floor(y);
+std::getline(std::cin >> std::ws, name);
+
+typedef std::vector<std::pair<std::string, int>> pairlist_t;
+typedef std::string text_t;
+typedef int number_t;
+
+cout << insertion opeator (삽입)
+cin >> extraction operator (추출)
+
+ */
+
+/*
+! type conversion
+Conversion a value of one data type to another
+* Implicit = automatic
+* Explicit = Precede value with new data type (int)
+
+ */
+/*
+  --> arithmetic operator
+  return the result of a specific arithemetic operation
+ */
+
+/*
+  --> 1. typedef
+  reserved keyword used to create an additional name (alias) for another data
+  type. New identifier for an existing type. Helps with readablility and reduces
+  typos use when there is a clear benefit Replaced with 'using' (work better w/
+  templates)
+
+ */
