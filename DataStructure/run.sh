5ee42347e3e138d18777b8bb7bd8b838dda91391
Kim Bum Jun <admin@vivabj.com>
2024-06-03 10:14:46 +0900

    Create new Temp/Example

---
+++ DataStructure/run.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env zsh
+
+clang++ -std=c++23 -g -Wall -o ./bin/Dst Main.cpp && ./bin/Dst
---
+++ Temp/Example/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Example" << std::endl;
+    return 0;
+}
