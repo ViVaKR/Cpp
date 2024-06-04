cf41c68d3b5177c93e18f5261544954afd348373
Kim Bum Jun <admin@vivabj.com>
2024-06-04 17:28:35 +0900

    Create and read Dijkstra

---
+++ Algorithm/Dijkstra/src/Main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    (1) dist 배열을 가중치 값들로 초기화 시킨다
+    (2) 시작점을 방문 처리 하고, 시작점에서 갈 수 있는 모든 정점들을 큐에 넣는다
+    (3) 큐에서 하나씩 꺼내면서, 해당 정점을 방문 처리하고, 해당 정점에서 갈 수 있는 모든 정점들을 큐에 넣는다
+    (4) 큐가 빌 때까지 반복한다
+
+    --> 방문하지 않은 노드 중 가장 작은 가중치를 가진 노드를 선택하여 방문하고,
+    --> 그 노드와 연결된 다른 노드들의 거리를 계산하여 dist 배열을 업데이트 시킨다
+
+     */
+
+    // Weight Matrix
+    // Visit
+    // Distance
+
+    cout << "Hello, World!" << std::endl;
+    return 0;
+} // namespace stdint main()
